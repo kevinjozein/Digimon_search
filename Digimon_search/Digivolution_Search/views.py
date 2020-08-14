61aa29c4eccca8506b16568e8c8512bf62f9b9a7
kevinjozein <kevinjozein@gmail.com>
2020-08-14 17:36:55 +0800

    add more codes

--- Digimon_search/Digivolution_Search/views.py
+++ Digimon_search/Digivolution_Search/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django import forms
 # Create your views here.
+class NewForm(forms.Form):
+    base_digimon = forms.CharField(label="Base Digimon")
+    end_digimon = forms.CharField(label="Base Digimon")
+
+def search(request):
+
+    form = NewForm()
+    return render(request,"search/search.html",{"form":form})
